4e8a3dd90638213eb3f8b24aaeafdc0c0b2ccd85
bhavana-tech <bhavanas096@gmail.com>
2024-10-18 13:09:09 +0530

    Update remove.list_lambda.py

--- Python Programs/Lists/remove.list_lambda.py
+++ Python Programs/Lists/remove.list_lambda.py
@@ -1,1 +1,6 @@
-list=[1423,3242,3,5,6,4,5654,33,'g']
+In python, we can use lambda function to filter out specific items based on condition. Only the elements that satisfy that condition are filtered
+
+Eg 1
+list1=[1423,3242,3,5,6,4,5654,33,'g']
+filtered_list=filter(lambda x:x<=10,list1)
+print(filtered_list)
