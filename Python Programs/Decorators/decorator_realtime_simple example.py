6703b00c8476bb5b67e9920f9dce57bc267fa016
bhavana-tech <bhavanas096@gmail.com>
2024-12-17 22:28:16 +0530

    Update decorator_realtime_simple example.py

--- Python Programs/Decorators/decorator_realtime_simple example.py
+++ Python Programs/Decorators/decorator_realtime_simple example.py
@@ -19,3 +19,34 @@ def other_next_food():
 next_food()
 print("----------------")
 other_next_food()
+
+---------------------------------------------------------------------------------
+universal_rule="asia"
+
+def deco_func(func):
+    def wrapper():
+        
+        enclosed_variable="india"
+        global universal_rule
+        universal_rule="india"
+        print("universal_rule in decoraotr",universal_rule)
+        
+        return func()
+    return wrapper
+    
+@deco_func    
+def outer_func():
+    global universal_rule
+    universal_rule="karnataka"
+    print("universal_rule in outer",universal_rule)
+    
+@deco_func    
+def inner_func():
+    global universal_rule
+    universal_rule="mysore"
+    print("universal_rule in inner",universal_rule)
+    
+inner_func()
+outer_func()
+
+    
