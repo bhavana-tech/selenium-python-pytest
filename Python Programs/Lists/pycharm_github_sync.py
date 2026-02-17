b00a4f408eaceb3a707b2dfb1c5bd52d935fad05
bhavana-tech <bhavanas096@gmail.com>
2026-02-17 17:00:28 +0530

    Update pycharm_github_sync.py

--- Python Programs/Lists/pycharm_github_sync.py
+++ Python Programs/Lists/pycharm_github_sync.py
@@ -5,3 +5,9 @@ git status
 git add List_Basic.py
 git commit -m "Updated List_Basic.py"
 git push origin master
+
+
+//If any update is done ingit hub  and the same code to reflect in pycharm IDE
+
+git pull origin master 
+OR pycharm IDE > Git > PULL
