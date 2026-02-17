16334d207f2e0084be36dcf6b304bce3869b434d
Bhavana <bhavanas096@gmail.com>
2026-02-17 16:37:05 +0530

    few

---
+++ Python Programs/Lists/pycharm_github_sync.py
@@ -0,0 +1,7 @@
+//If any update is done in pycharm IDE and the same code to reflect in GIT HUB(""
+ Follow below
+
+git status
+git add List_Basic.py
+git commit -m "Updated List_Basic.py"
+git push origin master
