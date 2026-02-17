968635bc4edf2713ccb91ef6d691d5c21d84684f
bhavana-tech <bhavanas096@gmail.com>
2026-02-17 16:49:02 +0530

    Update pycharm_github_sync.py

--- Python Programs/Lists/pycharm_github_sync.py
+++ Python Programs/Lists/pycharm_github_sync.py
@@ -5,3 +5,17 @@ git status
 git add List_Basic.py
 git commit -m "Updated List_Basic.py"
 git push origin master
+
+
+//If any update is done in Git hub and the same code to reflect in Pycharm IDE(""
+ Follow below
+
+ ENSURE YOU ARE IN SAME BRANCH - selenium-python-pytest ansd also in MASTER
+
+ 1. Make sure you are inside current project folder
+
+ ----GIT PULL ORIGIN master
+
+ If any error u get -- NOT A git repository, then you are in wrong branch.  
+
+ 
