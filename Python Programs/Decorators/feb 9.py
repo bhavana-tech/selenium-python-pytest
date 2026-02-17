3ede6d11fb846f1a928da51e30d6f6aadb4d36de
Bhavana <bhavanas096@gmail.com>
2026-02-17 16:50:21 +0530

    weffe

---
+++ Python Programs/Decorators/feb 9.py
@@ -0,0 +1,4 @@
+list=[234,343,23]
+print(list)
+
+tuple=
--- Python Programs/Lists/pycharm_github_sync.py
+++ Python Programs/Lists/pycharm_github_sync.py
@@ -1,7 +1,6 @@
-//If any update is done in pycharm IDE and the same code to reflect in GIT HUB(""
- Follow below
 
 git status
 git add List_Basic.py
 git commit -m "Updated List_Basic.py"
 git push origin master
+
